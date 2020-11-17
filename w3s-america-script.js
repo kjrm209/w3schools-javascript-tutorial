ee3cf519a9540295d5e58997538e9f450e92023f
kjrm209 <61448722+kjrm209@users.noreply.github.com>
2020-11-17 20:39:36 +0800

    17 November 2020 @ 8:39 PM: w3s-america-script.js

---
+++ w3s-america-script.js
@@ -0,0 +1,15 @@
+function mysecondFunction() {
+    document.getElementById("script-body-change-paragraph").innerHTML = "The Secondary Paragraph Is Changed."
+}
+
+
+/* Using Javascript Statements */
+
+var x, y, z
+x = 5
+y = 6
+z = x + y
+
+function algebraicFunction() {
+    document.getElementById('algebra-arithmetic-addition').innerHTML = "The value of Z is " + z + "."
+}
