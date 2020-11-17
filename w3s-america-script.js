f4c86e9bb84cc29dd4a6d20486a801feeb83894e
kjrm209 <61448722+kjrm209@users.noreply.github.com>
2020-11-17 23:01:12 +0800

    17 November 2020 @ 11:00 PM: w3s-america-script.js

--- w3s-america-script.js
+++ w3s-america-script.js
@@ -3,6 +3,7 @@ function mysecondFunction() {
 }
 
 
+
 /* Using Javascript Statements */
 
 var x, y, z
@@ -12,4 +13,12 @@ z = x + y
 
 function algebraicFunction() {
     document.getElementById('algebra-arithmetic-addition').innerHTML = "The value of Z is " + z + "."
+}
+
+
+
+/* https://www.w3schools.com/js/tryit.asp?filename=tryjs_statements_blocks */
+function myGreetingFunction() {
+    document.getElementById("first-greeting").innerHTML = "Hello Dolly!"
+    document.getElementById("second-greeting").innerHTML = "How are you?"
 }
