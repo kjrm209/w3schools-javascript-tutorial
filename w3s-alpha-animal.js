9122ff9eca1da1dfacb98dedcf4c9686f33732e2
kjrm209 <61448722+kjrm209@users.noreply.github.com>
2020-11-23 14:55:04 +0800

    23 Novembe 2020 @ 2:54 PM: w3s-alpha-animal.js

--- w3s-alpha-animal.js
+++ w3s-alpha-animal.js
@@ -4,6 +4,8 @@ https://www.w3schools.com/js/tryit.asp?filename=tryjs_whereto_head
 https://kjrm209.github.io/w3schools/w3s-js-whereto.html
 */
 
+/* Test 1 2 3. Delete Me Later! */
+
 function myfirstFunction() {
     document.getElementById("script-head-change-paragraph").innerHTML = "The First Paragraph Has Been Changed."
 }
