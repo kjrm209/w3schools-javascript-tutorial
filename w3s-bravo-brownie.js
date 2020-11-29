ae8311d4553d7f27405e43d6e67ed03fe930c5b0
kjrm209 <61448722+kjrm209@users.noreply.github.com>
2020-11-29 10:19:36 +0800

    29.11.2020 @ 10:19 AM: w3s-bravo-brownie.js

--- w3s-bravo-brownie.js
+++ w3s-bravo-brownie.js
@@ -13,7 +13,7 @@ var cars = ["Saab", "Volvo", "BMW", "Volkswagen"]
 document.getElementById("cars-indexes").innerHTML = cars[0]
 document.getElementById("index-maximum").innerHTML = cars.length
 
-
+//
 
 /* 
 // Javascript Examples ~ Javascript Arrays ~ Change An Array Element
@@ -27,3 +27,23 @@ fruits [0] = "Kiwi"
 fruits [2] = " " + "Pear"
 document.getElementById("fruits-changed").innerHTML = fruits
 
+//
+
+/*
+// JavaScript Examples - JavaScript Arrays - Loop Through An Array
+https://www.w3schools.com/js/tryit.asp?filename=tryjs_array_loop
+
+https://kjrm209.github.io/w3schools/w3s-bravo-arrays.html
+*/
+
+var fruits, text, flen, i
+fruits = ["Banana", "Orange", "Apple", "Mango"]
+flen = fruits.length
+
+text = "<ul>"
+for (i = 0; i < flen; i++) {
+    text += "<li>" + fruits[i] + "</li>"
+}
+text += "</ul>"
+
+document.getElementById("array-loop").innerHTML = text
