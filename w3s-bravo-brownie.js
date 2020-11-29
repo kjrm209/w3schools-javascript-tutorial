5301fe618d4d13f138fead292a36b82d5736ac86
kjrm209 <61448722+kjrm209@users.noreply.github.com>
2020-11-29 08:42:47 +0800

    29.11.2020 @ 8:42 AM: w3s-bravo-brownie.js

--- w3s-bravo-brownie.js
+++ w3s-bravo-brownie.js
@@ -1,1 +1,29 @@
+/* 
+// Javascript Examples ~ Javascript Arrays ~ Access An Array Element
+https://www.w3schools.com/js/tryit.asp?filename=tryjs_array_element
+
+// Javascript Examples ~ Javascript Arrays ~ Access A Full Array
+
+https://www.w3schools.com/js/tryit.asp?filename=tryjs_array_length
+
+https://kjrm209.github.io/w3schools/w3s-bravo-arrays.html
+*/
+
+var cars = ["Saab", "Volvo", "BMW", "Volkswagen"]
+document.getElementById("cars-indexes").innerHTML = cars[0]
+document.getElementById("index-maximum").innerHTML = cars.length
+
+
+
+/* 
+// Javascript Examples ~ Javascript Arrays ~ Change An Array Element
+https://www.w3schools.com/js/tryit.asp?filename=tryjs_array_change
+
+https://kjrm209.github.io/w3schools/w3s-bravo-arrays.html
+*/
+var fruits = ["Banana", " " + "Orange", " " + "Apple", " " + "Mango"]
+document.getElementById("fruits-original").innerHTML = fruits
+fruits [0] = "Kiwi"
+fruits [2] = " " + "Pear"
+document.getElementById("fruits-changed").innerHTML = fruits
 
