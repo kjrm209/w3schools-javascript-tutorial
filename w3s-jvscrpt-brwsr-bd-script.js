ec27a02eca71ff81828a6fc639039d265acb2a74
kjrm209 <61448722+kjrm209@users.noreply.github.com>
2020-12-14 19:17:00 +0800

    14.12.2020 @ 7:16 PM: w3s-jvscrpt-bd-script.js

--- w3s-jvscrpt-brwsr-bd-script.js
+++ w3s-jvscrpt-brwsr-bd-script.js
@@ -3,9 +3,10 @@
 https://kjrm209.github.io/w3schools/w3s-jvscrpt-brwsr-index.html
 */
 
+
 /*  JS Browser ~ Timing ~ Simple Timing
 https://www.w3schools.com/js/tryit.asp?filename=tryjs_timing1
 */
 function myGreetings() {
-    alert("hello")
+    alert("Hello!")
 }
