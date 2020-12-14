15cbfed8181dbfefc70b7c3118987a08bf02a8d4
kjrm209 <61448722+kjrm209@users.noreply.github.com>
2020-12-15 07:28:32 +0800

    15.12.2020 @ 7:28 AM: w3s-jvscrpt-brwsr-bd-script.js

--- w3s-jvscrpt-brwsr-bd-script.js
+++ w3s-jvscrpt-brwsr-bd-script.js
@@ -1,12 +1,45 @@
 // Do Not Remove !
 /* w3s-jvscrpt-brwsr-index.html
 https://kjrm209.github.io/w3schools/w3s-jvscrpt-brwsr-index.html
+
+W3Schools ~ JavaScript 
+JS Examples ~ JS Browser
+https://www.w3schools.com/js/js_ex_browser.asp
 */
-
-
+//
+//
+//
 /*  JS Browser ~ Timing ~ Simple Timing
 https://www.w3schools.com/js/tryit.asp?filename=tryjs_timing1
 */
+
 function myGreetings() {
     alert("Hello!")
 }
+//
+//
+//
+/*  JS Browser ~ Timing ~ Another Simple Timing
+https://www.w3schools.com/js/tryit.asp?filename=tryjs_timing2
+*/
+
+function timedText() {
+    setTimeout(myTimeout1, 2000)
+    setTimeout(myTimeout2, 4000)
+    setTimeout(myTimeout3, 6000)
+}
+
+function myTimeout1() {
+    document.getElementById('text-variable').innerHTML = "2 seconds have passed."
+}
+
+function myTimeout2() {
+    document.getElementById('text-variable').innerHTML = "4 seconds have passed."
+}
+
+function myTimeout3() {
+    document.getElementById('text-variable').innerHTML = "6 seconds have passed."
+}
+//
+//
+//
