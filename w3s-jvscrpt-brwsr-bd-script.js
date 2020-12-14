b9ca8f85ed5eb1a7d19f418c8d1a02748eeab61d
kjrm209 <61448722+kjrm209@users.noreply.github.com>
2020-12-14 19:05:33 +0800

    14.12.2020 @ 7:04 PM: w3s-jvscrpt-brwsr-bd-script.js

---
+++ w3s-jvscrpt-brwsr-bd-script.js
@@ -0,0 +1,11 @@
+// Do Not Remove !
+/* w3s-jvscrpt-brwsr-index.html
+https://kjrm209.github.io/w3schools/w3s-jvscrpt-brwsr-index.html
+*/
+
+/*  JS Browser ~ Timing ~ Simple Timing
+https://www.w3schools.com/js/tryit.asp?filename=tryjs_timing1
+*/
+function myGreetings() {
+    alert("hello")
+}
